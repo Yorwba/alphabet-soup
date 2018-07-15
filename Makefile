6919ed3ed3fd9b6c7a35da09153e06702d187eea
Yorwba <yorwb4@gmail.com>
2018-07-15 16:57:13 +0800

    Add Aozora to data sources

--- Makefile
+++ Makefile
@@ -24,3 +24,28 @@ data/tatoeba.sqlite: tatoeba_data.py $(TATOEBA_CSVS)
 data/tatoeba_sentences_%.csv: data/tatoeba.sqlite tatoeba_data.py
 	./tatoeba_data.py filter-language --database=$< \
 		--language=$* --minimum-level=5 > $@
+
+download-aozora-index:
+	wget --timestamping --directory-prefix=data/aozora/ \
+		https://www.aozora.gr.jp/index_pages/list_person_all_extended_utf8.zip
+
+data/aozora/list_person_%.zip:
+	wget --directory-prefix=data/aozora/
+		https://www.aozora.gr.jp/index_pages/list_person_$*.zip
+
+data/aozora/list_person_%.csv: data/aozora/list_person_%.zip
+	unzip -DD $< -d data/aozora/
+
+data/aozora/modern_works.urls: aozora_data.py data/aozora/list_person_all_extended_utf8.csv
+	./aozora_data.py modern-works > $@
+
+data/aozora/files: data/aozora/modern_works.urls
+	wget --timestamping --directory-prefix=data/aozora/files/ \
+		--input-file=$<
+	touch data/aozora/files
+
+data/aozora_sentences.csv: aozora_data.py data/aozora/files
+	./aozora_data.py extract-sentences > $@
+
+data/japanese_sentences.csv: data/tatoeba_sentences_jpn.csv data/aozora_sentences.csv
+	cat $^ > $@
---
+++ aozora_data.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+
+import argparse
+import csv
+
+
+with open('data/aozora/list_person_all_extended_utf8.csv') as f:
+    aozora_list = list(csv.reader(f))
+aozora_header = aozora_list[0]
+aozora_table = aozora_list[1:]
+
+
+def modern_works(args):
+    author_birth = aozora_header.index('生年月日')
+    author_death = aozora_header.index('没年月日')
+    text_url = aozora_header.index('テキストファイルURL')
+    modern_files = list(
+        row[text_url] for row in aozora_table
+        if row[author_birth].startswith('19')
+        and not row[author_death]
+        and row[text_url].strip().split('.')[-1] in ('zip', 'txt')
+        and (not args.aozora_only
+             or row[text_url].startswith('https://www.aozora.gr.jp/')))
+    for url in modern_files:
+        print(url)
+
+
+def extract_sentences(args):
+    import os
+    import re
+    import zipfile
+    filedir = 'data/aozora/files'
+    for filename in os.listdir(filedir):
+        filepath = os.path.join(filedir, filename)
+        if zipfile.is_zipfile(filepath):
+            try:
+                with zipfile.ZipFile(filepath) as z:
+                    for n in z.namelist():
+                        if n.endswith('.txt'):
+                            text = z.read(n).decode('shift-jis')
+                            lines = text.split('\r\n')
+                            separators = list(i for i, l in enumerate(lines)
+                                              if set(l) == {'-'})
+                            try:
+                                end = lines.index('［＃本文終わり］')
+                            except ValueError:
+                                continue
+                            text_lines = lines[separators[1]+1:end]
+                            sentence_count = 0
+                            for line in text_lines:
+                                line = re.sub('［＃[^］]*］', '', line)
+                                sentences = re.split('(「|。|」|『|』|？|！)', line)
+                                for i in range(0,len(sentences),2):
+                                    sentence = sentences[i]
+                                    if i+1 < len(sentences):
+                                        if sentences[i+1] in ('。', '？', '！'):
+                                            sentence += sentences[i+1]
+                                    sentence = sentence.strip()
+                                    if not sentence:
+                                        continue
+                                    print('\t'.join((
+                                        'aozora',
+                                        ':'.join((filename, n, str(sentence_count))),
+                                        sentence)))
+                                    sentence_count += 1
+            except zipfile.BadZipFile:
+                pass
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description='Tatoeba data file parser')
+    parser.add_argument('command', nargs=1, choices={
+        'modern-works',
+        'extract-sentences'})
+    parser.add_argument('--aozora-only', type=bool, default=True)
+    args = parser.parse_args(argv[1:])
+
+    globals()[args.command[0].replace('-', '_')](args)
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
